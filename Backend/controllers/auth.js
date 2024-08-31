556fccc3881b3e8777ad7ed4e2f9a728a6c94d20
Seifeldin-Ahmed <120275931+Seifeldin-Ahmed@users.noreply.github.com>
2024-08-31 03:10:01 +0300

    Update auth.js

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -8,6 +8,7 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transport = nodemailer.createTransport(sendgridTransport({
+    auth: {api_key: "" }
 }));
 
 
