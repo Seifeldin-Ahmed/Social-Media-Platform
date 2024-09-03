9ef32f7f196f5cfc32b2bfd2fd249dc4c8ffee70
Seifeldin-Ahmed <120275931+Seifeldin-Ahmed@users.noreply.github.com>
2024-09-03 03:06:35 +0300

    [models] update posts

--- Backend/models/post.js
+++ Backend/models/post.js
@@ -5,7 +5,7 @@ const postSchema = new Schema({
     caption: String,
     imageUrl: String,
     userId: {type: Schema.Types.ObjectId, required: true, ref:'User'},
-    sharedBy: {type: Schema.Types.ObjectId, required: true, ref:'User'}
+    sharedBy: {type: Schema.Types.ObjectId, ref:'User'}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Post', postSchema);
