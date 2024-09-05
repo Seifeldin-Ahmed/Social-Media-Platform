5390a928c1927e2227c3b0f10a517f44ca3c2ad0
Momen Hassan <128437881+MomenHassan12@users.noreply.github.com>
2024-09-05 16:55:35 +0200

    [frontend] add protected route

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
 const crypto = require('crypto');
@@ -8,14 +8,14 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transport = nodemailer.createTransport(sendgridTransport({
-    auth: {api_key: "" }
+    auth: { api_key: "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" }
 }));
 
 
-exports.signup = async (req, res, next) =>{
+exports.signup = async (req, res, next) => {
     const errors = validationResult(req);
-    try{
-        if(!errors.isEmpty()){
+    try {
+        if (!errors.isEmpty()) {
             const error = new Error('Validation failed.');
             error.statusCode = 422;
             error.data = errors.array(); //keep errors to send it back to the frontend
@@ -27,33 +27,33 @@ exports.signup = async (req, res, next) =>{
         const dateOfBirth = req.body.dateOfBirth;
         const firstName = req.body.firstName;
         const lastName = req.body.lastName;
-        let ProfilePic;  
-        if(!req.file){
-            if(gender === "female"){
+        let ProfilePic;
+        if (!req.file) {
+            if (gender === "female") {
                 ProfilePic = 'images/female.jpg';
             } else {
                 ProfilePic = 'images/male.jpg';
-            } 
+            }
         } else {
-            ProfilePic = req.file.path.replaceAll('\\','/');
+            ProfilePic = req.file.path.replaceAll('\\', '/');
         }
         const hashedPassword = await bcrypt.hash(password, 12);
-        const user = new User({firstName: firstName, lastName: lastName, email: email, password: hashedPassword, gender: gender, dateOfBirth: dateOfBirth, ProfilePic: ProfilePic})
+        const user = new User({ firstName: firstName, lastName: lastName, email: email, password: hashedPassword, gender: gender, dateOfBirth: dateOfBirth, ProfilePic: ProfilePic })
         const result = await user.save();
-        res.status(201).json({message: 'User created!', userId: result._id.toString()});
+        res.status(201).json({ message: 'User created!', userId: result._id.toString() });
     }
-    catch(err){
-        if(!err.statusCode){
-            err.statusCode = 500 ; 
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
         }
         next(err);
     }
 };
 
-exports.login = async (req, res, next) =>{
+exports.login = async (req, res, next) => {
     const errors = validationResult(req);
-    try{
-        if(!errors.isEmpty()){
+    try {
+        if (!errors.isEmpty()) {
             const error = new Error('Validation failed.');
             error.statusCode = 422;
             error.data = errors.array(); //keep errors to send it back to the frontend
@@ -61,24 +61,24 @@ exports.login = async (req, res, next) =>{
         }
         const email = req.body.email;
         const password = req.body.password;
-        const user = await User.findOne({email: email})
-        if(!user){
+        const user = await User.findOne({ email: email })
+        if (!user) {
             const error = new Error('A user with this email could not be found');
             error.statusCode = 401; // not authenticated
             throw error;
         }
-        const isEqual = await bcrypt.compare(password, user.password);    
-        if(!isEqual){
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
             const error = new Error('Wrong password!');
             error.statusCode = 401; // not authenticated
             throw error;
         }
-        const token = jwt.sign({email: user.email, userId: user._id.toString()}, 'strongsupersupersecretsecret');
-        res.status(200).json({message: 'login successfully', token: token, userId: user._id.toString()}); 
-    } 
-    catch(err){
-        if(!err.statusCode){
-            err.statusCode = 500 ; 
+        const token = jwt.sign({ email: user.email, userId: user._id.toString() }, 'strongsupersupersecretsecret');
+        res.status(200).json({ message: 'login successfully', token: token, userId: user._id.toString() });
+    }
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
         }
         next(err);
     }
@@ -87,22 +87,22 @@ exports.login = async (req, res, next) =>{
 
 exports.forgetPassword = (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         const error = new Error('Validation failed.');
         error.statusCode = 422;
         error.data = errors.array(); //keep errors to send it back to the frontend
         throw error;
     }
     crypto.randomBytes(3, async (err, buffer) => {
-        if(err){
+        if (err) {
             next(err);
             return;
         }
         let resetCode = parseInt(buffer.toString('hex'), 16) % 1000000;
         resetCode = resetCode.toString().padStart(6, '0');
         try {
-            const user = await User.findOne({email: req.body.email});
-            if(!user){
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
                 const error = new Error('A user with this email could not be found');
                 error.statusCode = 401; // not authenticated
                 throw error;
@@ -116,7 +116,7 @@ exports.forgetPassword = (req, res, next) => {
                 to: req.body.email,
                 from: 'system.service.mailer@gmail.com',
                 subject: 'Password reset',
-                html:`
+                html: `
                 <!DOCTYPE html>
                 <html>
                 <body style="font-family: Arial, sans-serif; background-color: #f9f9f9; margin: 0; padding: 0;">
@@ -147,11 +147,11 @@ exports.forgetPassword = (req, res, next) => {
                 </html>
               `
             });
-            res.status(200).json({message:'code sent', userId: result._id.toString()}); 
+            res.status(200).json({ message: 'code sent', userId: result._id.toString() });
         }
-        catch(err){
-            if(!err.statusCode){
-                err.statusCode = 500 ; 
+        catch (err) {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
             next(err);
         }
@@ -160,16 +160,16 @@ exports.forgetPassword = (req, res, next) => {
 
 exports.verifyCode = async (req, res, next) => {
     const errors = validationResult(req);
-    try{
-        if(!errors.isEmpty()){
+    try {
+        if (!errors.isEmpty()) {
             const error = new Error('Validation failed.');
             error.statusCode = 422;
             error.data = errors.array(); //keep errors to send it back to the frontend
             throw error;
         }
         const resetCode = req.body.resetCode;
-        const user = await User.findOne({email: req.body.email});
-        if(!user){
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
             const error = new Error('user not found');
             error.statusCode = 404;
             throw error;
@@ -180,11 +180,11 @@ exports.verifyCode = async (req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
-        res.status(200).json({message:'valid code', userId: result._id.toString()}); 
+        res.status(200).json({ message: 'valid code', userId: user._id.toString() });
     }
-    catch(err){
-        if(!err.statusCode){
-            err.statusCode = 500 ; 
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
         }
         next(err);
     }
@@ -192,16 +192,16 @@ exports.verifyCode = async (req, res, next) => {
 
 exports.setNewPassword = async (req, res, next) => {
     const errors = validationResult(req);
-    try{
-        if(!errors.isEmpty()){
+    try {
+        if (!errors.isEmpty()) {
             const error = new Error('Validation failed.');
             error.statusCode = 422;
             error.data = errors.array(); //keep errors to send it back to the frontend
             throw error;
         }
         const password = req.body.password;
-        const user = await User.findOne({email: req.body.email});
-        if(!user){
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
             const error = new Error('user not found');
             error.statusCode = 404;
             throw error;
@@ -211,11 +211,11 @@ exports.setNewPassword = async (req, res, next) => {
         user.resetCode = undefined;
         user.resetCodeExpiration = undefined;
         await user.save();
-        res.status(200).json({message:'password reset successfully', userId: result._id.toString()}); 
+        res.status(200).json({ message: 'password reset successfully', userId: user._id.toString() });
     }
-    catch(err){
-        if(!err.statusCode){
-            err.statusCode = 500 ; 
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
         }
         next(err);
     }
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 const router = express.Router();
 const User = require('../models/user');
 const authController = require('../controllers/auth');
 
 router.put('/signup',
     body('email').isEmail().withMessage('Plase enter a vaild email.').custom((value, { req }) => {
-        return User.findOne({email: value})
-               .then(userDoc => {
-                    if(userDoc){
-                        return Promise.reject('E-mail address already exists!');
-                    }
-               });
+        return User.findOne({ email: value })
+            .then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('E-mail address already exists!');
+                }
+            });
     }).normalizeEmail(),
-    body('password').trim().isLength({min: 7}).matches(/[A-Z]/).withMessage('at least one uppercase letter required').matches(/\d/).withMessage('at least one number required'),
-    body('confirmPassword').custom((value, {req}) => {
-        if(value !== req.body.password){
+    body('password').trim().isLength({ min: 7 }).matches(/[A-Z]/).withMessage('at least one uppercase letter required').matches(/\d/).withMessage('at least one number required'),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
             throw new Error('Passwords have to match!');
         }
         return true;
     }).trim(),
-    body('firstName').trim().not().isEmpty().isLength({min:3}),
-    body('lastName').trim().not().isEmpty().isLength({min:3}),
+    body('firstName').trim().not().isEmpty().isLength({ min: 3 }),
+    body('lastName').trim().not().isEmpty().isLength({ min: 3 }),
     body('gender').trim().not().isEmpty(),
     body('dateOfBirth').not().isEmpty().isDate().withMessage('Invalid date format. Please enter a valid date (YYYY-MM-DD).'),
     authController.signup
@@ -45,9 +45,9 @@ router.post('/verify',
 
 router.post('/set-new-password',
     body('email').isEmail().withMessage('please enter a valid email.').normalizeEmail(),
-    body('password').trim().isLength({min: 7}).matches(/[A-Z]/).withMessage('at least one uppercase letter required').matches(/\d/).withMessage('at least one number required'),
-    body('confirmPassword').custom((value, {req}) => {
-        if(value !== req.body.password){
+    body('password').trim().isLength({ min: 7 }).matches(/[A-Z]/).withMessage('at least one uppercase letter required').matches(/\d/).withMessage('at least one number required'),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
             throw new Error('Passwords have to match!');
         }
         return true;
